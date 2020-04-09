644b97218eee0bf3bd0ed1ba5007aca4c892820e
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-04-09 20:24:33 +0900

    [ADD] serviceIntroducePage

---
+++ src/Page/ServiceIntroducePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainTemplate from 'components/MainTemplate/MainTemplate';
+import ServiceIntroduce from 'components/ServiceIntroduce/ServiceIntroduce.js';
+
+const ServiceIntroducePage = () => {
+  return (
+    <MainTemplate>
+      <ServiceIntroduce />
+    </MainTemplate>
+  );
+};
+
+export default ServiceIntroducePage;
