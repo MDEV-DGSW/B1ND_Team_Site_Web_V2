cb42cc07adaecb7212ca6bfe6af1edc65e7501da
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-04-09 16:00:13 +0900

    [ADD] Admin Router
    Add Admin Router

---
+++ src/Page/AdminPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Admin from 'components/Admin/Admin';
+import MainTemplate from 'components/MainTemplate/MainTemplate';
+
+const AdminPage = () => {
+    return (
+        <MainTemplate>
+          <Admin/>
+        </MainTemplate>
+    );
+}
+
+export default AdminPage;
