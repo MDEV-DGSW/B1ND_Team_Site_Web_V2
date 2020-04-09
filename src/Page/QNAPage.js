b5166c1b05dac3197382c401ae7cb50c44164ab9
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-04-09 16:24:07 +0900

    [ADD]QNA Router
    Add QNA Router Link

---
+++ src/Page/QNAPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import QNA from 'components/QNA/QNA';
+import MainTemplate from 'components/MainTemplate/MainTemplate';
+
+const QNAPage = () => {
+    return (
+        <MainTemplate>
+          <QNA/>
+        </MainTemplate>
+    );
+}
+
+export default QNAPage;
