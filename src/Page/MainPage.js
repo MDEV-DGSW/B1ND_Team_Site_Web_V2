aa836979ed03abe96bad238553666de214727c36
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-04-09 15:55:23 +0900

    [ADD]Route
    Add Router, NavBar files, Main Template files, ServiceNotice files, Page folder

---
+++ src/Page/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from 'components/Main/Main';
+import MainTemplate from 'components/MainTemplate/MainTemplate';
+
+const MainPage = () => {
+    return (
+        <MainTemplate>
+          <Main/>
+        </MainTemplate>
+    );
+}
+
+export default MainPage;
