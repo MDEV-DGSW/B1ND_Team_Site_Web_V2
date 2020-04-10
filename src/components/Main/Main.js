238e3a9899ede07cd691edf576368b3874a4f513
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-04-10 16:17:33 +0900

    [ADD&FIX]Main
    메인 페이지 스타일링중입니다

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
+import './Main.scss';
+
+const MainTopItem = ({header, number, Increase, updateDate, linkName, link}) => {
+  return(
+    <li className="main__content-toplist-item">
+      <h1 className="main__content-toplist-item-header">{header}</h1>
+      <span className="main__content-toplist-item-number">{number}개</span>
+      <span className="main__content-toplist-item-increase">{Increase}</span>
+      <div className="main__content-toplist-item-updateDate">update:{updateDate}</div>
+      <div className="main__content-toplist-item-link">
+        <a href={link}>{linkName}</a>
+      </div>
+    </li>
+  );
+}
+
 const Main = () => {
   return(
-    <>
-      hello world Main Test
-    </>
+    <div className="main">
+    <div className="main__slide">
+      <div className="main__slide-title">Title text</div>
+      <div className="main__slide-content">content text here</div>
+    </div>
+
+    <div className="main__content">
+      <ul className="main__content-toplist">
+        <MainTopItem
+        header="코드 파일 수" number="202020"
+        updateDate="2020.04.11"
+        linkName="깃허브 바로가기" link="guthub.com"
+        />
+      </ul>
+    </div>
+    </div>
   );
 }
 
