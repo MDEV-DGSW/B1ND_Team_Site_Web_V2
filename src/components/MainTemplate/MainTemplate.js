a5b8b615d495d48ac34309c716743055033dffd1
하형 <51150005+maryoh2003@users.noreply.github.com>
2020-04-10 17:00:30 +0900

    [ADD] Footer
    푸터 작업 했습니다

--- src/components/MainTemplate/MainTemplate.js
+++ src/components/MainTemplate/MainTemplate.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import NavBar from 'components/Common/NavBar/NavBar';
+import NavBar from 'components/Common/NavBar';
+import Footer from 'components/Common/Footer';
+import classNames from 'classnames/bind';
+import style from './MainTemplate.scss';
 
-import './MainTemplate.scss';
+const cx = classNames.bind(style);
 
-function MainTemplate({ children }) {
-  return (
-    <div className="main-temp">
-      <div className="main-temp__body">
-        <div className="main-temp__nav">
-          <NavBar />
-        </div>
-        <div className="main-temp__content">
-          {children}
-        </div>
+const MainTemplate = ({children})=>{
+
+  return(
+    <div className={cx('MainTemplate')}>
+      <div className={cx('MainTemplate-header')}> 
+      <NavBar/>
+      </div>
+      <div className={cx('MainTemplate-contents')}>
+        {children}
+      </div>
+      <div className={cx('MainTemplate-footer')}>
+        <Footer/>
       </div>
     </div>
-  );
+  )
+
 }
 
 export default MainTemplate;
