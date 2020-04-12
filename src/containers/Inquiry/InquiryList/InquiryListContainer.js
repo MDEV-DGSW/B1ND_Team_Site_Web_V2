a4f3928dbfb7b786f5bf714de888860813ec2922
yiyb0603 <yiyb0603@dgsw.hs.kr>
2020-04-12 14:12:00 +0900

    [FIX] 문의 컨테이너 정리

--- src/containers/Inquiry/InquiryList/InquiryListContainer.js
+++ src/containers/Inquiry/InquiryList/InquiryListContainer.js
@@ -1,50 +1,10 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React from 'react';
 import { inject, observer } from 'mobx-react';
 import InquiryList from 'components/Inquiry/InquiryList';
-import Loading from 'components/Loading';
 
-const InquiryListContainer = ({ store }) => {
-  const { handleInquiryList, pageCount } = store.InquiryStore;
-  const [isLoading, setIsLoading] = useState(true);
-  const [page, setPage] = useState(1);
-  const [inquiryList, setInquiryList] = useState([]);
-
-  const requestInquiryList = useCallback(async () => {
-    await handleInquiryList(1)
-      .then (response => {
-        if (response.status === 200) {
-          setInquiryList(response.data.inquirys);
-        }
-        setIsLoading(false);
-      })
-
-      .catch (error => {
-        console.log(error);
-        setIsLoading(false);
-        return error;
-      })
-  }, [handleInquiryList]);
-
-  const pageCountUp = () => {
-    requestInquiryList();
-    setPage(page + 1);
-  }
-
-  const pageCountDown = () => {
-    setPage(page - 1);
-    requestInquiryList();
-  }
-
-  useEffect(() => {
-    requestInquiryList();
-  }, []);
-
+const InquiryListContainer = () => {
   return (
-    <>
-      {
-        isLoading ? <Loading /> : <InquiryList  />
-      }
-    </>
+    <InquiryList  />
   );
 }
 
