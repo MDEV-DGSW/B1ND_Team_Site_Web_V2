904161ca7bc896933906825327753abeec9f7250
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-04-12 20:21:02 +0900

    [ADD]Images
    슬라이드 이미지 보정본 추가

--- src/components/Common/ImageSlide/ImageSlide.js
+++ src/components/Common/ImageSlide/ImageSlide.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './ImageSlide.scss';
 import image1 from 'assets/images/이미지1.jpg';
 import image2 from 'assets/images/권용빈.png';
+import image3 from 'assets/images/이미지1.jpg';
+import image4 from 'assets/images/이미지1.jpg';
 import {Fade} from "react-slideshow-image";
  
 const slideImages = [
   image1,
   image2,
-  image1
+  image3,
+  image4
 ];
 
 const fadeProperties = {
@@ -49,7 +52,7 @@ const ImageSlide = () => {
         <div className="ImageSlide__slide" >
           <span className="ImageSlide__slide-img">
           <div className="ImageSlide__slide-img-4">
-              <img src ={slideImages[1]} className ="ImageSlide__slide-img-4-4" alt ="slideimage" />
+              <img src ={slideImages[3]} className ="ImageSlide__slide-img-4-4" alt ="slideimage" />
             </div>
           </span>
         </div>
