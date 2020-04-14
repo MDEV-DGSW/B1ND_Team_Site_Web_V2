2aece053981e671babe0dd6aebe858c5e0c74213
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-04-15 02:28:27 +0900

    [FIX]Main text
    예시텍스트 수정했습니다.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import { AiOutlineCaretUp } from "react-icons/ai";
-import { MdPlayArrow } from "react-icons/md"
+// import { AiOutlineCaretUp } from "react-icons/ai";
+// import { MdPlayArrow } from "react-icons/md"
 // import { AiFillCaretRight } from "react-icons/ai";
 // import { FaKiwiBird } from "react-icons/fa";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { FaChalkboardTeacher } from "react-icons/fa";
+import { FiBookOpen } from "react-icons/fi";
+import { FaHandsHelping } from "react-icons/fa";
 import ImageSlide from 'components/Common/ImageSlide/ImageSlide';
 
 import Image1 from "assets/images/이미지1.jpg";
@@ -24,9 +26,9 @@ const MainTopItem = ({ header, number, Increase, updateDate, linkName, link }) =
   return(
     <li className="main__content-toplist-item">
       <h1 className="main__content-toplist-item-header">{header}</h1>
-      <span className="main__content-toplist-item-number">{number}개</span>
+      <span className="main__content-toplist-item-number">{number}</span>
       <span className="main__content-toplist-item-increase">
-        <span className="main__content-toplist-item-increase-icon"><AiOutlineCaretUp/></span>{Increase}
+        {/* <span className="main__content-toplist-item-increase-icon"><AiOutlineCaretUp/></span>{Increase} */}
       </span>
       <div className="main__underline" /> {/*number 아래 선*/}
       <div className="main__content-toplist-item-updateDate">update:{updateDate}</div>
@@ -41,8 +43,10 @@ const MainBottomItem = ({ imageUrl, hoverTitle, hoverDes }) => {
     <div className="main__content-bottomList-item">
       <img className="main__content-bottomList-item-img" src={imageUrl} alt ="이미지" />
       <div className="main__content-bottomList-item-hoverContent">
-      <div className="main__content-bottomList-item-hoverContent-title">{hoverTitle}</div>
-      <span className="main__content-bottomList-item-hoverContent-icon" ><MdPlayArrow/></span>
+      <div className="main__content-bottomList-item-hoverContent-title">
+        <span style={{color:'#0067bc'}}>{hoverTitle}</span>
+        년의 바인드</div>
+      {/* <span className="main__content-bottomList-item-hoverContent-icon" ><MdPlayArrow/></span> */}
       <div className="main__content-bottomList-item-hoverContent-des">{hoverDes}</div>
       </div>
     </div>
@@ -68,28 +72,41 @@ const Main = () => {
       <ul className="main__content-toplist">
         <div className="main__content-toplist-wrap">
         <MainTopItem
-        header="코드 라인 수" 
-        number="1298276" Increase="1,000"
+        header="코드 라인" 
+        number="1298276줄" Increase="1,000"
         updateDate="2020.04.11"
-        linkName="깃허브 바로가기" link="guthub.com"
+        linkName="깃허브 바로가기" link="github.com"
         />
         <MainTopItem
-        header="코드 라인 수" 
-        number="1298276" Increase="1,000"
+        header="프로젝트 파일" 
+        number="5036개" Increase="1,000"
         updateDate="2020.04.11"
-        linkName="깃허브 바로가기" link="guthub.com"
+        linkName="깃허브 바로가기" link="github.com"
         />
         <MainTopItem
-        header="코드 라인 수" 
-        number="1298276" Increase="1,000"
+        header="생성한 API" 
+        number="47개" Increase="1,000"
         updateDate="2020.04.11"
-        linkName="깃허브 바로가기" link="guthub.com"
+        linkName="깃허브 바로가기" link="github.com"
+        />
+        <div className="main__content-toplist-underline" />
+        <MainTopItem
+        header="커밋 횟수" 
+        number="293846회" Increase="1,000"
+        updateDate="2020.04.11"
+        linkName="깃허브 바로가기" link="github.com"
         />
         <MainTopItem
-        header="코드 라인 수" 
-        number="1298276" Increase="1,000"
+        header="도담도담 개발 총 시간" 
+        number="384015시간" Increase="1,000"
         updateDate="2020.04.11"
-        linkName="깃허브 바로가기" link="guthub.com"
+        linkName="깃허브 바로가기" link="github.com"
+        />
+        <MainTopItem
+        header="코드 라인" 
+        number="1298276줄" Increase="1,000"
+        updateDate="2020.04.11"
+        linkName="깃허브 바로가기" link="github.com"
         />
         </div>
       </ul>
@@ -97,10 +114,10 @@ const Main = () => {
       {/* bottomList-------------- */}
       <ul className="main__content-bottomList">
         <div className="main__content-bottomList-wrap">
-        <MainBottomItem imageUrl={Image1} hoverTitle="2020 ICT" hoverDes="2020.04.10 Lorem"/>
-        <MainBottomItem imageUrl={Image2} hoverTitle="2020 예시" hoverDes="2020.04.10 Lorem"/>
-        <MainBottomItem imageUrl={Image3} hoverTitle="파일 구조 배운날" hoverDes="2020.04.10 Lorem"/>
-        <MainBottomItem imageUrl={Image4} hoverTitle="2020 예시" hoverDes="2020.04.10 Lorem"/>
+        <MainBottomItem imageUrl={Image1} hoverTitle="2018" hoverDes="#해커톤 #부스"/>
+        <MainBottomItem imageUrl={Image2} hoverTitle="2019" hoverDes="#여기뭐할까"/>
+        <MainBottomItem imageUrl={Image3} hoverTitle="2019" hoverDes="#선배특강 #바인드실"/>
+        <MainBottomItem imageUrl={Image4} hoverTitle="2019" hoverDes="#ICT융합컨퍼런스"/>
         <li className="main__content-bottomList-textBox">
           <div className="main__content-bottomList-textBox-content">
             <span className="main__content-bottomList-textBox-content-title">타이틀 내용 입니다. 우리는 바인드.</span>
@@ -111,9 +128,9 @@ const Main = () => {
               </span>
           </div>
         </li>
-        <MainBottomItem imageUrl={Image7} hoverTitle="2020 예시" hoverDes="2020.04.10 Lorem"/>
-        <MainBottomItem imageUrl={Image5} hoverTitle="2020 예시" hoverDes="2020.04.10 Lorem"/>
-        <MainBottomItem imageUrl={Image6} hoverTitle="2020 예시" hoverDes="2020.04.10 Lorem"/>
+        <MainBottomItem imageUrl={Image7} hoverTitle="2019" hoverDes="#ICT #도담도담"/>
+        <MainBottomItem imageUrl={Image5} hoverTitle="2019" hoverDes="#연말 #2019회고"/>
+        <MainBottomItem imageUrl={Image6} hoverTitle="2017" hoverDes="#M2SYS #B1ND"/>
         </div>
       </ul>
 
@@ -130,19 +147,19 @@ const Main = () => {
           <div className="main__content-endList-list-wrap">
           <MainEndItem icon={<FaChartBar/>} endTitle="연말 회고 시간" 
           endDes="한학기 또는 주요 행사가 끝난 후 팀원이 모여 "
-          endDes2ndline="회고 시간을 갖습니다.예시 텍스트"
+          endDes2ndline="팀을 돌아보는 회고 시간을 갖습니다."
           />
-          <MainEndItem icon={<FaChalkboardTeacher/>} endTitle="Open Seminar" 
+          <MainEndItem icon={<FaChalkboardTeacher/>} endTitle="오픈 세미나" 
           endDes="학생들이 주도적으로 오픈 세미나를 진행하며 "
-          endDes2ndline="다양한 개발 기술과 문장이 생각이 안나요"
+          endDes2ndline="다양한 개발 기술과 서비스를 공유합니다."
           />
-          <MainEndItem icon={<FaChartBar/>} endTitle="연말 회고 시간" 
-          endDes="한학기 또는 주요 행사가 끝난 후 팀원이 모여 "
-          endDes2ndline="회고 시간을 갖습니다.예시 텍스트"
+          <MainEndItem icon={<FiBookOpen/>} endTitle="자율적인 학습" 
+          endDes="바인드에서는 팀원 스스로가 주도적, 자율적으로"
+          endDes2ndline="기술을 익히고 사용하며 학습합니다."
           />
-          <MainEndItem icon={<FaChartBar/>} endTitle="연말 회고 시간" 
-          endDes="한학기 또는 주요 행사가 끝난 후 팀원이 모여 "
-          endDes2ndline="회고 시간을 갖습니다.예시 텍스트"
+          <MainEndItem icon={<FaHandsHelping/>} endTitle="수평적인 문화 " 
+          endDes="선배, 후배와 선생님 모두 같은 눈높이에서"
+          endDes2ndline="커뮤니케이션하며 팀을 이끌어 나갑니다."
           />
           </div>
         </ul>
