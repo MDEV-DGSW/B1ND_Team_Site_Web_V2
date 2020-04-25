2da754637405f98454ea3acc07faa267956464fa
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-04-26 02:16:52 +0900

    [FIX]main top
    일단 전부 더한 결과로 적어뒀습니다.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -73,26 +73,26 @@ const Main = () => {
         <div className="main__content-toplist-wrap">
         <MainTopItem
         header="코드 라인" 
-        number="146495줄" Increase="1,000"
+        number="136,111줄" Increase="1,000"
         updateDate="2020.04.22"
         linkName="깃허브 바로가기" link="github.com"
         />
         <MainTopItem
         header="프로젝트 파일" 
-        number="506파일" Increase="1,000"
+        number="1,626개" Increase="1,000"
         updateDate="2020.04.22"
         linkName="깃허브 바로가기" link="github.com"
         />
         <MainTopItem
         header="생성한 API" 
-        number="47개" Increase="1,000"
+        number="255개" Increase="1,000"
         updateDate="2020.04.22"
         linkName="깃허브 바로가기" link="github.com"
         />
         <div className="main__content-toplist-underline" />
         <MainTopItem
         header="커밋 횟수" 
-        number="293846회" Increase="1,000"
+        number="4,586회" Increase="1,000"
         updateDate="2020.04.22"
         linkName="깃허브 바로가기" link="github.com"
         />
