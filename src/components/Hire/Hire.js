4afaabf71a7f793bfd8580522862e4b18d2ea96d
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-11-04 20:06:04 +0900

    [UPDATE] 5th Hire
    [UPDATE] - 5기 수시 채용으로 내용 변경

--- src/components/Hire/Hire.js
+++ src/components/Hire/Hire.js
@@ -10,71 +10,87 @@ const Hire = () => {
     <>
       <div>
         <div className={cx('ServiceNoticeTemplate-header')}>
-          <div className={cx('ServiceNoticeTemplate-wrap-logo')}></div>
+          <div className={cx('ServiceNoticeTemplate-wrap-logo')}>
+            {/* <img className={cx('ServiceNoticeTemplate-wrap-logo-img')} src={DODAM_LOGO} alt={'dodamdodam_logo'} onClick={() => {
+            handleRemoveLS();
+            history.push('/');
+          }}/> */}
+          </div>
         </div>
         <div className={cx('ServiceNoticeTemplate-contents')}>
           <div className={cx('ServiceNoticeTemplate-header-title')}>
             <div className={cx('ServiceNoticeTemplate-header-title-text')}>
-              바인드 채용 결과 안내
+              바인드 채용 공고
             </div>
             <div className={cx('ServiceNoticeTemplate-header-title-sub')}>
-              2020년 5기 채용에 지원해 주신 모든 분들께 감사의 인사를 전합니다.
-              <br />
+              더 나은 학교를 함께 만들어갈 역량 있는 팀원을 모집합니다.
             </div>
           </div>
 
           <div className={cx('ServiceNoticeTemplate-content-wrap')}>
             <div>
-              <b>&nbsp;</b>
-            </div>
-            <div>안녕하세요, B1ND 팀입니다.</div>
-            <div>
-              <b>2020 전반기 5기 공개 채용 결과에 대해 안내드립니다.</b>
+              <b>[일정 안내]</b>
             </div>
             <div>
               <b>&nbsp;</b>
             </div>
+            <div> &nbsp;- 지원자 접수 : 2020년 11월 25일 21:00 까지</div>
             <div>
-              <b>인턴으로 바인드와 함께 할 학생들은 아래와 같습니다</b>
+              &nbsp;- 면접 일정 : 추후 안내
             </div>
             <div>
-              <b>&nbsp;</b>
+              &nbsp;- 결과 발표 : 2020년 12월 2일 (채용 페이지 안내 랜덤)
             </div>
             <div>
-              &nbsp;<b>서버팀</b> : 박지나
-            </div>
-            <div>
-              &nbsp;<b>모바일팀</b> : 류한규
-            </div>
-            <div>
-              &nbsp;<b>윈도우팀</b> : 박세은, 임경준
-            </div>
-            <div>
-              &nbsp;<b>웹팀</b> : 홍준혁
+              &nbsp;- 모집 인원 : 10여명 내외
             </div>
             <div>
               <b>&nbsp;</b>
             </div>
             <div>
+              <b>[신청 안내]</b>
+            </div>
+            <div>&nbsp; </div>
+            <div>아래 이메일로 학년 / 반, 번호/ 이름을 적어서 보내주세요.</div>
+            <div>
+              <a className="Hire-Email" href="mailto:eun3673@dgsw.hs.kr">
+                eun3673@dgsw.hs.kr
+              </a>
+            </div>
+            <div>
               <b>&nbsp;</b>
             </div>
             <div>
-              <b>[인턴 프로세스 안내]</b>
               <div>
-                <b>&nbsp;</b>
+                <b>[제목]</b> <br /> 바인드 5기 공채지원_이영은
+              </div>
+              <div>
+                <div>
+                  <b>&nbsp;</b>
+                </div>
+                <b>[내용]</b> <br />
+                학번 : 2215 <br />
+                이름 : 이영은 <br />
+                지원하고 싶은 분야 : (예시: Web/Server/Windows/Android/iOS/Design ... ) <br />
               </div>
             </div>
             <div>
-              &nbsp;<b> - 기간 </b> : 2020.08.03 ~ 2020.09.28
+              <b>&nbsp;</b>
             </div>
             <div>
-              &nbsp;<b> - 내용 </b> : 본인이 지원한 분야에서 팀 프로젝트를
-              진행합니다.
+              <b>&nbsp;</b>
             </div>
-            {/* <div>
-              &nbsp;<b> - 내용 </b> : 주어지는 주제로, 각 플랫폼이 모여 하나의
-              토이 프로젝트를 진행합니다.
-            </div> */}
+            <div>
+              <b>[지원 자격]</b>
+            </div>
+            <div style={{color: 'red'}}>*5기 공채와 달리 이번 수시 선발은 실력 위주 선발입니다.</div>
+            <div style={{fontWeight: 'bold'}}> &nbsp;- 지원 분야에 대한 이해도가 높은 사람</div>
+            <div> &nbsp;- 실행력이 강한 사람</div>
+            <div> &nbsp;- 새로운 도전을 좋아하는 사람</div>
+            <div> &nbsp;- 팀원과 소통을 중요하게 생각하는 사람</div>
+            <div> &nbsp;- 팀워크를 사랑하는 사람</div>
+            <div> &nbsp;- 팀의 성장이 나의 성장이라고 생각하는 사람</div>
+            <div> &nbsp;- 팀과 함께 성장해 나갈 사람</div>
             <div>
               <b>&nbsp;</b>
             </div>
@@ -82,26 +98,20 @@ const Hire = () => {
               <b>&nbsp;</b>
             </div>
             <div>
-              <b>[합격자 대상 추가 안내사항]</b>
-              <div>
-                <b>&nbsp;</b>
-              </div>
+              <b>[5기 채용 관련 사항]</b>
             </div>
             <div>
-              &nbsp; -{' '}
-              <b>
-                일요일(8/2) 15:00에 프로그래밍1실에서 입사와 관련된 안내가 있을
-                예정입니다.
-              </b>
+              <b>&nbsp;</b>
+            </div>
+            <div>&nbsp; - 고용형태 : 인턴</div>
+            <div>
+              &nbsp; - 인턴십 수행 후, 우수 성과자에 대해 팀원으로 발령 날 수
+              있습니다.
             </div>
             <div>
-              &nbsp; - <b>바인드팀은 본인의 의사를 존중합니다. </b>
-              바인드 입사의 최종 결정은 부담없이 본인이 판단하시면 되며,
+              &nbsp; - 겨울방학 이전까지 인턴십 수행 후, 우수 성과자에 한해 팀원으로 발령 됩니다.
             </div>
-            <div>
-              바인드에 입사결정이 확고하신 분들은
-              <b> 일요일 입사 안내에 참여하시면 됩니다.</b>
-            </div>
+            <div> &nbsp; - 바인드 팀 2020년 마지막 공채입니다.</div>
           </div>
         </div>
       </div>
