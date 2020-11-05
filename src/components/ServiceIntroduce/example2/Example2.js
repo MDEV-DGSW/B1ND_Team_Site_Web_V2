2573d4460995871205f70d9a20cd34eaa338c8ae
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-05 09:15:39 +0900

    [ADD] example Teacher page
    overflow hidden 을 넣어서 밑에 있는 마름모를 어떤식으로 만들지 논의 해야함

---
+++ src/components/ServiceIntroduce/example2/Example2.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Example2.scss';
+import FadeIn from 'react-fade-in';
+import { useInView } from 'react-intersection-observer';
+const Example2 = () => {
+  const [ref, inView, entry] = useInView({
+    threshold: 0.1,
+    trackVisibility: true,
+    delay: 100,
+    initialInView: true,
+  });
+  console.log('Example2', inView);
+  console.log('');
+
+  return (
+    <div className="Example2" ref={ref}>
+      {inView ? (
+        <>
+          <div>
+            <FadeIn transitionDuration={400} delay={400}>
+              <div className={'Example2-square'} />
+              <div className="Example2-wrap">
+                <div className="Example2-wrap-left">a</div>
+                <div className="Example2-wrap-right">
+                  <div className="Example2-wrap-right-mobile"></div>
+                  <div className="Example2-wrap-right-web"></div>
+                </div>
+              </div>
+            </FadeIn>
+          </div>
+        </>
+      ) : null}
+    </div>
+  );
+};
+
+export default Example2;
