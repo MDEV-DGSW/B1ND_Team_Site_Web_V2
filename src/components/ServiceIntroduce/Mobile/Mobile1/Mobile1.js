fb9b6007b863bdb8e15bde76ec54225c7f6c8543
yiyb0603 <yiyb0603@gmail.com>
2020-11-09 19:55:45 +0900

    [ADD] Default Mobile1

---
+++ src/components/ServiceIntroduce/Mobile/Mobile1/Mobile1.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './Mobile1.scss';
+import { IoMdArrowDropleft } from 'react-icons/io';
+import { MdArrowDropDown } from 'react-icons/md';
+
+const Mobile1 = () => {
+  return (
+    <div className ="Mobile1">
+      <div className ="Mobile1-Title">
+        <div>도담도담 모바일 자랑하는 텍스트</div>
+        <div>뭐라고 쓸지 생각이 안난다</div>
+      </div>
+
+      <div className ="Mobile1-Middle">
+        <div className ="Mobile1-Middle-LeftSection">
+          <div className ="Mobile1-Middle-LeftSection-Title">
+            <div className ="Mobile1-Middle-LeftSection-Title-Name">
+              <div>위치 신청</div>
+            </div>
+
+            <div className ="Mobile1-Middle-LeftSection-Title-Image"></div>
+          </div>
+
+          <div className ="Mobile1-Middle-LeftSection-Title">
+            <div className ="Mobile1-Middle-LeftSection-Title-Name">
+              <div>분실물</div>
+            </div>
+
+            <div className ="Mobile1-Middle-LeftSection-Title-Image"></div>
+          </div>
+        </div>
+
+        <div className ="Mobile1-Middle-RightSection">
+          <div className ="Mobile1-Middle-RightSection-Title">학교 생활을 더 쉽게 & 빠르게</div>
+
+          <div className ="Mobile1-Middle-RightSection-SpeechBubble">
+            <IoMdArrowDropleft className ="Mobile1-Middle-RightSection-SpeechBubble-Arrow" />
+            <div className ="Mobile1-Middle-RightSection-SpeechBubble-Box"></div>
+          </div>
+
+          <div className ="Mobile1-Middle-RightSection-Contents">
+            <div>학생들의 곁에는 도담도담</div>
+            <div>내용들 서비스에 대한 어쩌고 저쩌고</div>
+            <div>간단한 소개와 텍스트들과 내용들 어쩌고 저쩌고</div>
+            <br />
+
+            <div>내용들 서비스에 대한 어쩌고 저쩌고</div>
+            <div>간단한 소개와 텍스트들과 내용들 어쩌고 저쩌고</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Mobile1;
---
+++ src/components/ServiceIntroduce/Mobile/Mobile2/Mobile2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Mobile2.scss';
+
+const Mobile2 = () => {
+  return (
+    <div className ="Mobile2">
+      <div></div>
+    </div>
+  );
+};
+
+export default Mobile2;
