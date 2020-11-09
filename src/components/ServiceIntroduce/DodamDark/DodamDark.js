c862c81429273e988782f29c43e6d7231dc2d754
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-09 20:47:52 +0900

    [ADD] DodamDark

--- src/components/ServiceIntroduce/DodamDark/DodamDark.js
+++ src/components/ServiceIntroduce/DodamDark/DodamDark.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import './DodamDark.scss';
 
 const DodamDark = () => {
   return (
     <>
-      <div className="DodamDark">도담 다크</div>
+      <div className="DodamDark">
+        <div className="DodamDark-explainWrap">
+          <div className="DodamDark-explainWrap-topTitle">
+            다크모드에 대한 설명 설명 <br />
+            모바일 / 웹에서 라이트 모드와 다크모드를 지원합니다.
+          </div>
+          <div className="DodamDark-explainWrap-underTitle primary">
+            도담도담 라이트모드🌙
+          </div>
+        </div>
+        <div className="DodamDark-imgWrap">
+          <div className="DodamDark-imgWrap-wrapper">
+            <div className="DodamDark-imgWrap-wrapper-mobile"></div>
+            <div className="DodamDark-imgWrap-wrapper-web"></div>
+            <div className="DodamDark-imgWrap-wrapper-mobile"></div>
+            <div className="DodamDark-imgWrap-wrapper-mobile"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
