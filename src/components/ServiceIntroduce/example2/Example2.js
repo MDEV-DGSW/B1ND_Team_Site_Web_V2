a4034b386f9aa54255efb4c98407396c913f94c8
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-09 19:03:25 +0900

    [ADD] LocationApply

--- src/components/ServiceIntroduce/example2/Example2.js
+++ src/components/ServiceIntroduce/example2/Example2.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Example2.scss';
 import FadeIn from 'react-fade-in';
 import { useInView } from 'react-intersection-observer';
-const Example2 = () => {
+const Example2 = ({ FirstPageInView }) => {
   const [ref, inView, entry] = useInView({
     threshold: 0.1,
     trackVisibility: true,
@@ -13,24 +13,31 @@ const Example2 = () => {
   console.log('');
 
   return (
-    <div className="Example2" ref={ref}>
-      {inView ? (
-        <>
-          <div>
-            <FadeIn transitionDuration={400} delay={400}>
-              <div className={'Example2-square'} />
-              <div className="Example2-wrap">
-                <div className="Example2-wrap-left">a</div>
-                <div className="Example2-wrap-right">
-                  <div className="Example2-wrap-right-mobile"></div>
-                  <div className="Example2-wrap-right-web"></div>
+    <>
+      {FirstPageInView ? null : (
+        <FadeIn transitionDuration={400} delay={90}>
+          <div className={'Example2-square'} />
+        </FadeIn>
+      )}
+
+      <div className="Example2" ref={ref}>
+        {inView ? (
+          <>
+            <div>
+              <FadeIn transitionDuration={400} delay={400}>
+                <div className="Example2-wrap">
+                  <div className="Example2-wrap-left">a</div>
+                  <div className="Example2-wrap-right">
+                    <div className="Example2-wrap-right-mobile"></div>
+                    <div className="Example2-wrap-right-web"></div>
+                  </div>
                 </div>
-              </div>
-            </FadeIn>
-          </div>
-        </>
-      ) : null}
-    </div>
+              </FadeIn>
+            </div>
+          </>
+        ) : null}
+      </div>
+    </>
   );
 };
 
