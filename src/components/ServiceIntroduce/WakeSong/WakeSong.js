1149097038008ba255be48807ff9057498d0525a
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-09 20:03:10 +0900

    [ADD] Dodam WakeSong

---
+++ src/components/ServiceIntroduce/WakeSong/WakeSong.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './WakeSong.scss';
+
+const WakeSong = () => {
+  return (
+    <>
+      <div className="WakeSong">
+        <div className="WakeSong-section">02</div>
+        <div className="WakeSong-locationApplyWrap">
+          <div className="WakeSong-locationApplyWrap-topTitle">
+            듣고싶은 노래를 직접 신청
+          </div>
+          <div className="WakeSong-locationApplyWrap-underTitle primary">
+            아침 기상곡
+          </div>
+        </div>
+        <div className="WakeSong-locationApplyWrap-explainWrap">
+          <div className="WakeSong-locationApplyWrap-explainWrap-wrapper">
+            도담도담이 있기 전, 우리 학교는 <br /> 일과가 끝나면 모두 교무실에
+            모여 종이 당직을 작성했습니다.
+            <br /> 잃어버리고, 찢어지고, 지워지던 종이 당직 대신 <br />
+            이제는 클릭 몇번으로 손쉽게 위치신청!
+          </div>
+        </div>
+        <div className="WakeSong-locationApplyWrap-uiImgWrap">
+          <div className="WakeSong-locationApplyWrap-uiImgWrap-wrapper">
+            <div className="WakeSong-locationApplyWrap-uiImgWrap-wrapper-webImg"></div>
+            <div className="WakeSong-locationApplyWrap-uiImgWrap-wrapper-mobileImg"></div>
+            <div className="WakeSong-locationApplyWrap-uiImgWrap-wrapper-mobileImg"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default WakeSong;
