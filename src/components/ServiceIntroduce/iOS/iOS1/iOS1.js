6c65b42a1239582799860ce20a676a1bd4f4d7ce
yiyb0603 <yiyb0603@gmail.com>
2020-11-10 08:53:48 +0900

    [ADD] IOS1 Screen

--- src/components/ServiceIntroduce/iOS/iOS1/iOS1.js
+++ src/components/ServiceIntroduce/iOS/iOS1/iOS1.js
@@ -3,7 +3,25 @@ import './iOS1.scss';
 
 const IOS1 = () => {
   return (
-    <div>ios 첫번째 화면</div>
+    <div className ="IOS1">
+      <div className ="IOS1-LeftSection">
+        <div className ="IOS1-LeftSection-Image1"></div>
+        <div className ="IOS1-LeftSection-Image2"></div>
+      </div>
+
+      <div className ="IOS1-RightSection">
+        <div className ="IOS1-RightSection-Title">도담도담 iOS</div>
+        <div className ="IOS1-RightSection-Contents">
+          <div>학생들의 곁에는 항상 도담도담</div>
+          <div>내용을 서비스에 대한 어쩌고 저쩌고</div>
+          <div>간단한 소개와 텍스트들과 내용을 어쩌고 저쩌고</div>
+          
+          <br />
+          <div>내용을 서비스에 대한 어쩌고 저쩌고</div>
+          <div>간단한 소개와 텍스트들과 내용을 어쩌고 저쩌고</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
