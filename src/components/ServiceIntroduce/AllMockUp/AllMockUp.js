4d2ede116cc24bdab877fe166161d0e22b66b938
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-10 10:09:38 +0900

    [ADD] allMockup component

---
+++ src/components/ServiceIntroduce/AllMockUp/AllMockUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './AllMockUp.scss';
+
+const AllMockUp = () => {
+  return (
+    <>
+      <div>목업 떄려 박을 화면</div>
+    </>
+  );
+};
+
+export default AllMockUp;
