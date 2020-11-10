a8fe8728588e38bd1ec1328d21c25d21dfd51293
yiyb0603 <yiyb0603@gmail.com>
2020-11-10 13:37:49 +0900

    [ADD] IOS Fadein

--- src/components/ServiceIntroduce/iOS/iOS1/iOS1.js
+++ src/components/ServiceIntroduce/iOS/iOS1/iOS1.js
@@ -1,26 +1,42 @@
 import React from 'react';
 import './iOS1.scss';
+import FadeIn from 'react-fade-in';
+import { useInView } from 'react-intersection-observer';
 
 const IOS1 = () => {
-  return (
-    <div className ="IOS1">
-      <div className ="IOS1-LeftSection">
-        <div className ="IOS1-LeftSection-Image1"></div>
-        <div className ="IOS1-LeftSection-Image2"></div>
-      </div>
+  const [ref, inView] = useInView({
+    threshold: 0.1,
+    trackVisibility: true,
+    delay: 100,
+    initialInView: true,
+  });
 
-      <div className ="IOS1-RightSection">
-        <div className ="IOS1-RightSection-Title">도담도담 iOS</div>
-        <div className ="IOS1-RightSection-Contents">
-          <div>학생들의 곁에는 항상 도담도담</div>
-          <div>내용을 서비스에 대한 어쩌고 저쩌고</div>
-          <div>간단한 소개와 텍스트들과 내용을 어쩌고 저쩌고</div>
-          
-          <br />
-          <div>내용을 서비스에 대한 어쩌고 저쩌고</div>
-          <div>간단한 소개와 텍스트들과 내용을 어쩌고 저쩌고</div>
+  return (
+    <div style ={{ backgroundColor: '#0067bc' }}>
+    {
+      inView &&
+        <FadeIn transitionDuration={400} delay={400}>
+        <div ref={ref} className ="IOS1">
+          <div className ="IOS1-LeftSection">
+            <div className ="IOS1-LeftSection-Image1"></div>
+            <div className ="IOS1-LeftSection-Image2"></div>
+          </div>
+
+          <div className ="IOS1-RightSection">
+            <div className ="IOS1-RightSection-Title">도담도담 iOS</div>
+            <div className ="IOS1-RightSection-Contents">
+              <div>학생들의 곁에는 항상 도담도담</div>
+              <div>내용을 서비스에 대한 어쩌고 저쩌고</div>
+              <div>간단한 소개와 텍스트들과 내용을 어쩌고 저쩌고</div>
+              
+              <br />
+              <div>내용을 서비스에 대한 어쩌고 저쩌고</div>
+              <div>간단한 소개와 텍스트들과 내용을 어쩌고 저쩌고</div>
+            </div>
+          </div>
         </div>
-      </div>
+        </FadeIn>
+      }
     </div>
   );
 };
--- src/components/ServiceIntroduce/iOS/iOS2/iOS2.js
+++ src/components/ServiceIntroduce/iOS/iOS2/iOS2.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './iOS2.scss';
 import LocationBallon from 'assets/images/BallonSpeech/Location.png';
+import FadeIn from 'react-fade-in';
+import { useInView } from 'react-intersection-observer';
 
 const IOS2Item = ({ topic }) => {
   return (
@@ -14,17 +16,31 @@ const IOS2Item = ({ topic }) => {
 };
 
 const IOS2 = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.1,
+    trackVisibility: true,
+    delay: 100,
+    initialInView: true,
+  });
+  
   const topics = [LocationBallon, LocationBallon, LocationBallon, LocationBallon, LocationBallon];
 
   return (
-    <div className ="IOS2">
-      <div className ="IOS2-ItemWrapper">
+    <div>
       {
-        topics.map((topic) => {
-          return <IOS2Item topic ={topic} />
-        })
+        inView &&
+        <FadeIn transitionDuration={400} delay={400}>
+          <div ref={ref} className ="IOS2">
+            <div className ="IOS2-ItemWrapper">
+            {
+              topics.map((topic) => {
+                return <IOS2Item topic ={topic} />
+              })
+            }
+            </div>
+          </div>
+        </FadeIn>
       }
-      </div>
     </div>
   );
 };
