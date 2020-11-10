2c14c21f2dc6456e9d12e36a8456aa089f3e4ccb
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-11-11 00:17:19 +0900

    [ADD] Min Service SubPage
    [ADD] - 마인 기능 소개 페이지 작업했습니다.

---
+++ src/components/ServiceIntroduce/MineService/MineServiceSub/MineServiceSub.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./MineServiceSub.scss";
+
+const MineServiceSub = () => {
+  return (
+    <div className="MineServiceSub">
+      <div className="MineServiceSub__content">
+        <h1>MINE FROM B1nd</h1>
+        <div className="MineServiceSub__content-serviceList">
+          <ul>
+            <li>기능 소개 제목</li>
+            <li>
+              <p>학생들의 곁에는 항상 도담도담<br/>
+              어쩌고ㅈ 저쩌고어저고
+              </p>
+              <p>
+                내용들 서비스에대한 어저고저<br/>
+                간단한 소개와 텍스트들과<br/>
+                내용들 학생들의 곁에는
+              </p>
+            </li>
+          </ul>
+
+          <ul>
+            <li>기능 소개 제목</li>
+            <li>
+              <p>학생들의 곁에는 항상 도담도담<br/>
+              어쩌고ㅈ 저쩌고어저고
+              </p>
+              <p>
+                내용들 서비스에대한 어저고저<br/>
+                간단한 소개와 텍스트들과<br/>
+                내용들 학생들의 곁에는
+              </p>
+            </li>
+          </ul>
+
+          <ul>
+            <li>기능 소개 제목</li>
+            <li>
+              <p>학생들의 곁에는 항상 도담도담<br/>
+              어쩌고ㅈ 저쩌고어저고
+              </p>
+              <p>
+                내용들 서비스에대한 어저고저<br/>
+                간단한 소개와 텍스트들과<br/>
+                내용들 학생들의 곁에는
+              </p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="MineServiceSub__img">
+        <img alt="mine" />
+      </div>
+    </div>
+  )
+}
+
+export default MineServiceSub;
