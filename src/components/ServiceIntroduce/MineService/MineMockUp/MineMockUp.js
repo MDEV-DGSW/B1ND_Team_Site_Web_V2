73b39c9238caee23155b614dcc51b3f1858041b2
권순관 <48983361+SoonGwan@users.noreply.github.com>
2020-11-12 19:54:17 +0900

    [ADD] MinMockup

--- src/components/ServiceIntroduce/MineService/MineMockUp/MineMockUp.js
+++ src/components/ServiceIntroduce/MineService/MineMockUp/MineMockUp.js
@@ -1,34 +1,30 @@
-import React from "react";
-import "./MineMockUp.scss";
+import React from 'react';
+import './MineMockUp.scss';
+import TeacherSecondPage from 'assets/images/mockup/Teacher_SecondPage.png';
 
 const MineMockUp = () => {
   return (
     <div className="MineMockUp">
-      <div className="MineMockUp__img">
-        <img alt="mine mockup img" />
+      <div className="MineMockUp-imgWrap">
+        <img src={TeacherSecondPage} alt={TeacherSecondPage} />
       </div>
-      <div className="MineMockUp__content">
-        <h1><span style={{color: '#0067bc'}}>여기</span> 들어갈 내용</h1>
-        <div className="MineMockUp__content-description">
-          <p className="MineMockUp__content-description-title">기능 소개 제목</p>
-          <p>학생들의 곁에는 항상 도담도담<br/>
-          어쩌고ㅈ 저쩌고어저고
-          </p>
-          <p>내용들 서비스에대한 어저고저<br/>
-          간단한 소개와 텍스트들과<br/>
-          내용들 학생들의 곁에는
-          </p>
-          <p>학생들의 곁에는 항상 도담도담<br/>
-          어쩌고ㅈ 저쩌고어저고
-          </p>
-          <p>내용들 서비스에대한 어저고저<br/>
-          간단한 소개와 텍스트들과<br/>
-          내용들 학생들의 곁에는
-          </p>
+      <div className="MineMockUp-textWrap">
+        <div className="MineMockUp-textWrap-topTitle primary">
+          여기 글자 <br /> 이렇게
+          <br /> 넣으면 <br /> 예쁠듯
+        </div>
+        <div className="MineMockUp-textWrap-underTitle">
+          학생들의 곁에는 항상 도담도담 <br />
+          어쩌고 저쩌고어ㅉ
+          <br />
+          <br />
+          에 대한 어쩌고 저쩌고어ㅉ
+          <br />
+          한 소개와 텍스트들과 내용들 <br />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default MineMockUp;
