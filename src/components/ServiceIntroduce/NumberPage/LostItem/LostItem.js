89212673952f68187e3c561e239c6787cec43899
Mary Oh <maryoh2002@gmail.com>
2020-11-12 22:19:23 +0900

    Add: NumberPages

--- src/components/ServiceIntroduce/NumberPage/LostItem/LostItem.js
+++ src/components/ServiceIntroduce/NumberPage/LostItem/LostItem.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './LostItem.scss';
 import FadeIn from 'react-fade-in';
 import { useInView } from 'react-intersection-observer';
-import LostItemWeb from 'assets/images/mockup/LostItem.png';
-
+import WebLostFound from 'assets/images/mockup/Web_LostFound.png';
+import MobileLostFound from 'assets/images/mockup/Mobile_LostFound.png';
+import MobileLostFoundComment from 'assets/images/mockup/Mobile_LostFoundComment.png';
 const LostItem = () => {
   const [ref, inView] = useInView({
     threshold: 0.1,
@@ -20,27 +21,27 @@ const LostItem = () => {
               <div className="LostItem-section">04</div>
               <div className="LostItem-locationApplyWrap">
                 <div className="LostItem-locationApplyWrap-topTitle">
-                  일과 후 시간 위치 확인을 위한
+                  빠르고 쉽게 찾는
                 </div>
                 <div className="LostItem-locationApplyWrap-underTitle primary">
-                  분실물
+                  분실물, 습득물
                 </div>
               </div>
               <div className="LostItem-locationApplyWrap-explainWrap">
                 <div className="LostItem-locationApplyWrap-explainWrap-wrapper">
-                  도담도담이 있기 전, 우리 학교는 <br /> 일과가 끝나면 모두
-                  교무실에 모여 종이 당직을 작성했습니다.
-                  <br /> 잃어버리고, 찢어지고, 지워지던 종이 당직 대신 <br />
-                  이제는 클릭 몇번으로 손쉽게 위치신청!
+                  도담도담이 있기 전, 우리 학교는 <br /> SNS를 통해 분실물을 공유했습니다
+                  <br /> 도담도담의 분실물, 습득물 게시판을 통해 <br />
+                  빠르고 쉽게 잃어버린 물건을 찾아가세요!
                 </div>
               </div>
               <div className="LostItem-locationApplyWrap-uiImgWrap">
                 <div className="LostItem-locationApplyWrap-uiImgWrap-wrapper">
                   <div className="LostItem-locationApplyWrap-uiImgWrap-wrapper-webImg">
-                    <img src={LostItemWeb} alt={LostItemWeb} />
+                    <img src={WebLostFound} alt={WebLostFound} />
                   </div>
-                  <div className="LostItem-locationApplyWrap-uiImgWrap-wrapper-mobileImg"></div>
-                  <div className="LostItem-locationApplyWrap-uiImgWrap-wrapper-mobileImg"></div>
+                  <div className="LostItem-locationApplyWrap-uiImgWrap-wrapper-mobileImg">
+                    <img src={MobileLostFound} alt={MobileLostFound} />
+                  </div>
                 </div>
               </div>
             </div>
--- src/components/ServiceIntroduce/NumberPage/Offbase/Offbase.js
+++ src/components/ServiceIntroduce/NumberPage/Offbase/Offbase.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './Offbase.scss';
 import FadeIn from 'react-fade-in';
 import { useInView } from 'react-intersection-observer';
-import OffbaseWeb from 'assets/images/mockup/Offbase.png';
+import WebMain from 'assets/images/mockup/Web_Main.png';
+import MobileOffBase from 'assets/images/mockup/Mobile_OffBase.png';
+import MobileOffBaseApply from 'assets/images/mockup/Mobile_OffBaseApply.png';
 
 const Offbase = () => {
   const [ref, inView] = useInView({
@@ -20,27 +22,28 @@ const Offbase = () => {
               <div className="Offbase-section">02</div>
               <div className="Offbase-locationApplyWrap">
                 <div className="Offbase-locationApplyWrap-topTitle">
-                  일과 후 시간 위치 확인을 위한
+                  손쉬운 신청과 복귀를 위한
                 </div>
                 <div className="Offbase-locationApplyWrap-underTitle primary">
-                  외출 외박
+                  외출 / 외박 신청
                 </div>
               </div>
               <div className="Offbase-locationApplyWrap-explainWrap">
                 <div className="Offbase-locationApplyWrap-explainWrap-wrapper">
-                  도담도담이 있기 전, 우리 학교는 <br /> 일과가 끝나면 모두
-                  교무실에 모여 종이 당직을 작성했습니다.
-                  <br /> 잃어버리고, 찢어지고, 지워지던 종이 당직 대신 <br />
-                  이제는 클릭 몇번으로 손쉽게 위치신청!
+                  도담도담이 있기 전, 우리 학교는 <br /> 외출/외박을 신청하고 복귀할 때 직접
+                  선생님께 찾아가 확인받았습니다
+                  <br /> 번거롭게 선생님을 찾으러 가는 대신 <br />
+                  편하고 간단하게 신청하세요!
                 </div>
               </div>
               <div className="Offbase-locationApplyWrap-uiImgWrap">
                 <div className="Offbase-locationApplyWrap-uiImgWrap-wrapper">
                   <div className="Offbase-locationApplyWrap-uiImgWrap-wrapper-webImg">
-                    <img src={OffbaseWeb} alt={OffbaseWeb} />
+                    <img src={WebMain} alt={WebMain} />
                   </div>
-                  <div className="Offbase-locationApplyWrap-uiImgWrap-wrapper-mobileImg"></div>
-                  <div className="Offbase-locationApplyWrap-uiImgWrap-wrapper-mobileImg"></div>
+                  <div className="Offbase-locationApplyWrap-uiImgWrap-wrapper-mobileImg">
+                    <img src={MobileOffBase} alt={MobileOffBase} />
+                  </div>
                 </div>
               </div>
             </div>
