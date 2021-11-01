c13c77d5a802bb6507bad9e795ecd5c9f99f5cfd
Clzzi <clzzi1109@naver.com>
2021-11-01 15:29:32 +0900

    Fix: 줄바꿈, 폰트 굵기

--- src/components/ServiceIntroduce/MoramFunction/MoramFunction.js
+++ src/components/ServiceIntroduce/MoramFunction/MoramFunction.js
@@ -8,21 +8,35 @@ import MoramPostView from "assets/images/mockup/MoramMobilePostview.png";
 const MoramFunction = () => {
   const imageArray = [
     { intro: "프로필을 설정해서", image: MoramProfile },
-    { intro: "현재 가장 인기있는 모집글 중에서", image: MoramMain },
-    { intro: "마음에 드는 모집글을 선택하여", image: MoramPostView },
+    { intro: `현재 가장 인기있는`, intro2: "모집글 중에서", image: MoramMain },
+    { intro: "마음에 드는", intro2: "모집글을 선택하여", image: MoramPostView },
     { intro: "간편하게 지원해보세요!", image: MoramApplication },
   ];
   return (
     <div className="MoramFunction">
       <div className="MoramFunction-ImgWrap">
-        {imageArray.map((img) => {
-          return (
-            <div className="MoramFunction-ImgWrap-Content">
-              <div className="MoramFunction-ImgWrap-Content-Intro">{img.intro}</div>
-              <img src={img.image} alt={img.image} />
-            </div>
-          );
-        })}
+        <div className="MoramFunction-ImgWrap-Content">
+          <div className="MoramFunction-ImgWrap-Content-Intro">{imageArray[0].intro}</div>
+          <img src={imageArray[0].image} alt={imageArray[0].image} />
+        </div>
+        <div className="MoramFunction-ImgWrap-Content">
+          <div className="MoramFunction-ImgWrap-Content-Intro" style={{ margin: 0 }}>
+            {imageArray[1].intro}
+          </div>
+          <div className="MoramFunction-ImgWrap-Content-Intro">{imageArray[1].intro2}</div>
+          <img src={imageArray[1].image} alt={imageArray[1].image} />
+        </div>
+        <div className="MoramFunction-ImgWrap-Content">
+          <div className="MoramFunction-ImgWrap-Content-Intro" style={{ margin: 0 }}>
+            {imageArray[2].intro}
+          </div>
+          <div className="MoramFunction-ImgWrap-Content-Intro">{imageArray[2].intro2}</div>
+          <img src={imageArray[2].image} alt={imageArray[2].image} />
+        </div>
+        <div className="MoramFunction-ImgWrap-Content">
+          <div className="MoramFunction-ImgWrap-Content-Intro">{imageArray[0].intro}</div>
+          <img src={imageArray[3].image} alt={imageArray[3].image} />
+        </div>
       </div>
     </div>
   );
