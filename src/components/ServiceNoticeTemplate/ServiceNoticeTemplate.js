29245b56c197c233643565ec5fa76c783b010a26
pjn141414 <jina071049@gmail.com>
2021-11-03 17:41:35 +0900

    add file

--- src/components/ServiceNoticeTemplate/ServiceNoticeTemplate.js
+++ src/components/ServiceNoticeTemplate/ServiceNoticeTemplate.js
@@ -1,32 +1,34 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import classNames from 'classnames/bind';
-import style from './ServiceNoticeTemplate.scss';
-import DODAM_LOGO from 'assets/icon/dodamLogo.svg';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import classNames from "classnames/bind";
+import style from "./ServiceNoticeTemplate.scss";
+import DODAM_LOGO from "assets/icon/dodamLogo.svg";
 
 const cx = classNames.bind(style);
 
 const ServiceNoticeTemplate = ({ title, subtitle, content, history }) => {
-  
   return (
     <div>
-      <div className={cx('ServiceNoticeTemplate-header')}>
-        <div className={cx('ServiceNoticeTemplate-wrap-logo')}>
-          <img className={cx('ServiceNoticeTemplate-wrap-logo-img')} src={DODAM_LOGO} alt={'dodamdodam_logo'} onClick={() => {
-            history.push('/');
-          }}/>
+      <div className={cx("ServiceNoticeTemplate-header")}>
+        <div className={cx("ServiceNoticeTemplate-wrap-logo")}>
+          <img
+            className={cx("ServiceNoticeTemplate-wrap-logo-img")}
+            src={DODAM_LOGO}
+            alt={"dodamdodam_logo"}
+            onClick={() => {
+              history.push("/");
+            }}
+          />
         </div>
       </div>
-      <div className={cx('ServiceNoticeTemplate-contents')}>
-        <div className={cx('ServiceNoticeTemplate-header-title')}>
-          <div className={cx('ServiceNoticeTemplate-header-title-text')}>{title}</div>
-          <div className={cx('ServiceNoticeTemplate-header-title-sub')}>{subtitle}</div>
+      <div className={cx("ServiceNoticeTemplate-contents")}>
+        <div className={cx("ServiceNoticeTemplate-header-title")}>
+          <div className={cx("ServiceNoticeTemplate-header-title-text")}>{title}</div>
+          <div className={cx("ServiceNoticeTemplate-header-title-sub")}>{subtitle}</div>
         </div>
-        <div className={cx('ServiceNoticeTemplate-content-wrap')}>
-          <div className={cx('ServiceNoticeTemplate-content-text')}>
-            {content}
-          </div>
+        <div className={cx("ServiceNoticeTemplate-content-wrap")}>
+          <div className={cx("ServiceNoticeTemplate-content-text")}>{content}</div>
         </div>
       </div>
     </div>
@@ -36,11 +38,7 @@ const ServiceNoticeTemplate = ({ title, subtitle, content, history }) => {
 ServiceNoticeTemplate.propTypes = {
   title: PropTypes.string,
   subtitle: PropTypes.string,
-  content: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.node
-  ]),
-
+  content: PropTypes.oneOfType([PropTypes.string, PropTypes.node]),
 };
 
 export default withRouter(ServiceNoticeTemplate);
