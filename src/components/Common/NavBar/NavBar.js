e169934d9c9d38355c1ed53419899c343c6315ed
developersihyeon <139722653+developersihyeon@users.noreply.github.com>
2024-06-15 17:33:33 +0900

    DEL :: DGSW 로고 삭제

--- src/components/Common/NavBar/NavBar.js
+++ src/components/Common/NavBar/NavBar.js
@@ -26,12 +26,10 @@ const NavBar = ({ pageType, history }) => {
       <div className={cx("NavBar-wrap")}>
         <nav className={cx("NavBar-wrap-nav")}>
           <Link to={"/"}>
-            <img src={DGSWLOGO} alt="logo" className={cx("NavBar-wrap-nav-wrap-logo")} />
             <img
               src={LOGO}
               alt="logo"
               className={cx("NavBar-wrap-nav-wrap-logo")}
-              style={{ width: "37px", left: "6%",top: '24%' }}
             />
             <span className={cx("NavBar-wrap-nav-wrap-logo-text")}></span>
           </Link>
